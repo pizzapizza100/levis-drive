16f40dda1c1b20bdd28d618b6519066939695350
pizzapizza100 <nadavlevi99@gmail.com>
2025-02-09 01:00:52 +0200

    Adding a warn to main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 // main.rs
 
-use log::{debug, error, info};
+use log::{debug, error, info, warn};
 
 fn main() {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init(); // Initialize the logger implementation
 
-    info!("This is an info message");
     debug!("This is a debug message");
+    info!("This is an info message");
+    warn!("This is an warning message");
     error!("This is an error message");
 }
 
